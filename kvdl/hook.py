2d0b02ef4f0fa7363d6ca7c189504debdb87ab1d
Chris Culy <chris.culy@faithlife.com>
2024-03-18 08:29:52 -0500

    Add support for Bitnami system hooks (used by RabbitMQ Azure nodes).

--- kvdl/hook.py
+++ kvdl/hook.py
@@ -6,19 +6,25 @@ class Hook:
     self.action = action
     self.service = service + '.service'
 
-  def _systemd_action(self):
-    bus = dbus.SystemBus()
-    systemd = bus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
-    manager = dbus.Interface(systemd, 'org.freedesktop.systemd1.Manager')
-
-    if self.action == 'reload':
-      managerMethod = manager.ReloadUnit
-    elif self.action == 'restart':
-      managerMethod = manager.RestartUnit
+  def _action(self):
+    if self.action == 'bitnami_restart':
+      # bitnami hook (https://docs.bitnami.com/aws/infrastructure/rabbitmq/administration/control-services/)
+      import subprocess
+      subprocess.run(['sudo', '/opt/bitnami/ctlscript.sh', 'restart', self.service])
     else:
-      sys.exit('Action must be "reload" or "restart"')
+      # systemd hook
+      bus = dbus.SystemBus()
+      systemd = bus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
+      manager = dbus.Interface(systemd, 'org.freedesktop.systemd1.Manager')
 
-    managerMethod(self.service, 'replace')
+      if self.action == 'reload':
+        managerMethod = manager.ReloadUnit
+      elif self.action == 'restart':
+        managerMethod = manager.RestartUnit
+      else:
+        sys.exit('Action must be "reload" or "restart"')
+
+      managerMethod(self.service, 'replace')
 
   def run(self):
-    self._systemd_action()
+    self._action()
